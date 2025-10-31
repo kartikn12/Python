6e463c1864df11bfb4a1436aad20e98c8abcc792
Kartik Nagpurkar <155724189+kartikn12@users.noreply.github.com>
2025-10-31 21:27:51 +0530

    Update gussname.py

--- mini_game/gussname.py
+++ mini_game/gussname.py
@@ -8,27 +8,18 @@ print("------Gusess the word------")
 
 computer_word= choice[0]+"_"*(len(choice)-2)+choice[-1]
 
-
 print("Word:",computer_word)
 
 for i in range(1,4):
-    
     print(f"-------round {i}-------")
-    
     user=input("Gusess Your word:")
     user=user.lower()
-    
     if choice==user:
         print("Your answer is correct")
-    elif choice<user:
+        break
+    elif user in choice or choice in user:
         print("You are too close")
-    # elif choice>user:
-    #     print("You are too far")
-    
     else:
         print("Better luck next time")
-        break
-        
-        
-    
-    
+else:
+    print("The correct word was:", choice)
